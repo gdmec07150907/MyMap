e1ba4eacdcbfa484d61297e77f2ce766e4078889
gdmec07150907 <570941644@qq.com>
2017-01-01 10:54:42 +0800

    Initial commit

---
+++ app/src/main/java/com/example/chenpc/mymapp/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.chenpc.mymapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.home_pc.mymap.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
